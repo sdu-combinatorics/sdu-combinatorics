a639a8f4d9a1b9e5cdc94abf51d1728482bdcab3
LongLee220284 <57667560+LongLee220284@users.noreply.github.com>
2025-12-09 15:16:46 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards', 'projects', 'teaching', 'talks']
+const section_names = ['home', 'publications', 'students', 'projects', 'teaching', 'talks']
 
 
 window.addEventListener('DOMContentLoaded', event => {
